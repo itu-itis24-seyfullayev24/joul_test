e66a6ddb1eba93051de3de809bf66460f1a20a51
Ravan-Seyfullayev <rseyfullayev26@gmail.com>
2024-06-22 23:04:24 +0400

    fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
-const nextConfig = { output: "standalone" };
+
+const nextConfig = {
+  output: "standalone", // existing configuration
+  export: "true", // new property to be added
+};
 
 export default withNextIntl(nextConfig);
